fff1ea5bc240bda92c015cbcd6fea0f79adcaf7c
Tell396 <vova21473@gmail.com>
2022-09-24 22:01:57 +1000

    Story #3. Update functional.

--- main.go
+++ main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonURL("example.com", "http://example.com"),
-		tgbotapi.NewInlineKeyboardButtonSwitch("2sw", "open 2"),
-		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
-	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
-		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
-		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
-	),
-)
-
 var replyKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("1"),
-		tgbotapi.NewKeyboardButton("2"),
-		tgbotapi.NewKeyboardButton("3"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("4"),
-		tgbotapi.NewKeyboardButton("5"),
-		tgbotapi.NewKeyboardButton("6"),
+		tgbotapi.NewKeyboardButton("Calatog"),
+		tgbotapi.NewKeyboardButton("My profile"),
+		tgbotapi.NewKeyboardButton("About this bot"),
 	),
 )
 
@@ -52,7 +35,7 @@ func main() {
 
 	for update := range updates {
 		if update.CallbackQuery != nil {
-			// fmt.Print(update)
+			fmt.Print(update)
 
 			bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data))
 			bot.Send(tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data))
@@ -61,12 +44,12 @@ func main() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
 			switch update.Message.Command() {
-			case "open":
-				msg.ReplyMarkup = numericKeyboard
-
 			case "start":
 				msg.Text = update.Message.From.FirstName + ", welcome to LoremMarket!"
 				msg.ReplyMarkup = replyKeyboard
+
+			default:
+				msg.Text = "Invalid response!"
 			}
 
 			bot.Send(msg)
